8a3315aef96acce5af689fe457b7b96b704afc1d
masotnikov <masotnikov11@yandex.ru>
2025-04-17 14:35:52 +0300

    feat: add interfaces for API response

---
+++ src/types/news.types.ts
@@ -0,0 +1,31 @@
+// интерфейс для данных о новостях
+export interface IData_SnippetNews {
+  ID: number;
+  TI: string;
+  AB: string;
+  URL: string;
+  DOM: string;
+  DP: string;
+  LANG: string;
+  REACH: number;
+  KW: IData_TagItem[];
+  AU: string[];
+  CNTR: string;
+  CNTR_CODE: string;
+  SENT: string;
+  TRAFFIC: IData_TrafficItem[];
+  FAV: string;
+  HIGHLIGHTS: string[];
+}
+
+// тэг для сниппета
+export interface IData_TagItem {
+  value: string;
+  count: number;
+}
+
+// траффик для сниппета
+export interface IData_TrafficItem {
+  value: string;
+  count: number;
+}
