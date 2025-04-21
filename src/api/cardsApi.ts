6cbd8530ac7ec3a6ec24613cbf4993a5cc66961c
masotnikov <masotnikov11@yandex.ru>
2025-04-21 10:14:19 +0300

    feat: add cardsApi.ts

---
+++ src/api/cardsApi.ts
@@ -0,0 +1,8 @@
+export const fetchCards = async () => {
+  const response = await fetch('/db.json');
+  if (!response.ok) {
+    throw new Error('Failed to load cards');
+  }
+  const data = await response.json();
+  return data.cards;
+};
