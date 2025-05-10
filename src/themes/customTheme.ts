0694929d5c44849b6de22bb30d5ae10b4ee7b3cc
masotnikov <masotnikov11@yandex.ru>
2025-05-10 23:56:31 +0300

    config: add theme config and tokens

--- src/themes/customTheme.ts
+++ src/themes/customTheme.ts
@@ -1,7 +1,24 @@
-export const customTheme = {
+import type {ThemeConfig} from "antd";
+
+const primaryColor: string = "#1a73e8";
+const secondaryColor: string = "#c3c0c0";
+
+export const customTheme: ThemeConfig = {
   token: {
-    colorPrimary: '#1890ff',
-    colorBgBase: '#141414',
-    colorTextBase: '#ffffff',
+    colorText: "#f4f3f3",
+    colorPrimary: primaryColor,
+    colorTextSecondary: secondaryColor
+  },
+  components: {
+    Typography: {
+        colorTextHeading: primaryColor,
+        colorLink: primaryColor
+    },
+    Collapse: {
+      colorTextHeading: primaryColor
+    },
+    Tag: {
+      colorTextSecondary: secondaryColor
+    }
   },
 };
