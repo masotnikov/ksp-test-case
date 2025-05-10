b4e9950c06d09ce983f92a05b28d92f6f2ae36a7
masotnikov <masotnikov11@yandex.ru>
2025-05-10 22:50:18 +0300

    feat(component): add TrafficPanel.tsx, TrafficPanel.module.scss

---
+++ src/components/TrafficPanel/TrafficPanel.tsx
@@ -0,0 +1,33 @@
+import {Row, Space, Tag, Typography} from "antd";
+import {IData_SnippetNews, IData_TrafficItem} from "../../types/news.types.ts";
+import style from "./TrafficPanel.module.scss"
+
+interface TrafficPanelProps {
+  snippetNews: IData_SnippetNews
+}
+export const TrafficPanel = ({snippetNews}: TrafficPanelProps) => {
+  const {DP, REACH, TRAFFIC, SENT} = snippetNews;
+  const {Text} = Typography;
+
+  return (
+    <Row wrap align={"middle"} justify={"space-between"}>
+      <Space wrap>
+        <Text>{DP}</Text>
+        <Text>{REACH} Reach</Text>
+        <Text>Top Traffic: </Text>
+        {TRAFFIC.map((trafficItem: IData_TrafficItem) => (
+          <Text key={trafficItem.value}>
+            {trafficItem.value} {trafficItem.count}
+          </Text>
+        ))}
+      </Space>
+      <Space wrap>
+        <Tag className={`${style.snippetTag} ${SENT === "negative" ? style.negativeTag : style.positiveTag}`}>
+          {SENT}
+        </Tag>
+      </Space>
+    </Row>
+
+  );
+};
+
