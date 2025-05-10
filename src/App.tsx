5c748e95fadb6dcb37fb80dec6f5f76b15403657
masotnikov <masotnikov11@yandex.ru>
2025-05-11 00:11:36 +0300

    refactor(component)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
+import {ConfigProvider, Spin} from "antd";
+import {useSnippets} from "./hooks/useSnippets.tsx";
+import {CardItem} from "./components/CardItem/CardItem";
+import "./index.scss";
+import {customTheme} from "./themes/customTheme.ts";
 
 function App() {
+  const {snippets, loading, error} = useSnippets();
+
+  if (loading) {
+    return (
+      <div className="app__spinner">
+        <Spin size="large"/>
+      </div>
+    );
+  }
+
+  if (error) return <div>Error: {error}</div>;
 
   return (
-    <>
-      <h1>Work</h1>
-    </>
-  )
+    <div className="app">
+      <ConfigProvider theme={customTheme}>
+        {snippets.map((snippet) => (
+          <CardItem key={snippet.ID} snippetNews={snippet}/>
+        ))}
+      </ConfigProvider>
+    </div>
+
+  );
 }
 
-export default App
+export default App;
