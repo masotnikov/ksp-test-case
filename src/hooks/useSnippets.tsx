0e5eeaf76636de8624e03c5c79246ca99776f1eb
masotnikov <masotnikov11@yandex.ru>
2025-05-10 23:43:33 +0300

    feat(hook): add useSnippets.tsx

--- src/hooks/useSnippets.tsx
+++ src/hooks/useSnippets.tsx
@@ -2,17 +2,17 @@ import {useEffect, useState} from "react";
 import {fetchCards} from "../api/cardsApi.ts";
 import {IData_SnippetNews} from "../types/news.types.ts";
 
-export const useCards = () => {
-  const [cards, setCards] = useState<IData_SnippetNews[]>([]);
+export const useSnippets = () => {
+  const [snippets, setSnippets] = useState<IData_SnippetNews[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     fetchCards()
-      .then(setCards)
+      .then(setSnippets)
       .catch(setError)
       .finally(() => setLoading(false))
   }, []);
 
-  return {cards, loading, error}
+  return {snippets: snippets, loading, error}
 }
