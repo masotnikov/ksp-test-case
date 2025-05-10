539b8c049506da99ea08a60e82817d2bd27a4645
masotnikov <masotnikov11@yandex.ru>
2025-05-10 22:45:49 +0300

    feat(component): add CardItem.tsx, CardItem.scss

--- src/components/CardItem/CardItem.tsx
+++ src/components/CardItem/CardItem.tsx
@@ -1,26 +1,28 @@
 import {Card, Space, Typography} from "antd";
-import {IData_SnippetNews} from "../../types/news.types.ts";
+import "/node_modules/flag-icons/css/flag-icons.min.css";
 import "./CardItem.scss";
+import {IData_SnippetNews} from "../../types/news.types.ts";
+import {TrafficPanel} from "../TrafficPanel/TrafficPanel.tsx";
+import {MainPanel} from "../MainPanel/MainPanel.tsx";
+import {Duplicates} from "../Duplicates/Duplicates.tsx";
 
-
-interface CardProps {
-  data: IData_SnippetNews;
+interface CardItemProp {
+  snippetNews : IData_SnippetNews
 }
 
-export const CardItem = ({data}: CardProps) => {
+export const CardItem = ({snippetNews}: CardItemProp) => {
+  const {Title, Link} = Typography;
+  const {TI} = snippetNews;
+  console.log(snippetNews);
+
   return (
     <Card className="card-item">
       <Space direction={"vertical"} size={"small"}>
-        <Space wrap>
-          <Typography.Text>{data.DP}</Typography.Text>
-          <Typography.Text>{data.REACH} Reach</Typography.Text>
-          {
-            data.TRAFFIC.map(trafficItem => (
-              <Typography.Text>Top Traffic: {trafficItem.value} {trafficItem.count}</Typography.Text>
-            ))
-          }
-
-        </Space>
+        <TrafficPanel snippetNews={snippetNews}/>
+        <Title level={4}>{TI}</Title>
+        <MainPanel snippetNews={snippetNews}/>
+        <Link>Original Source</Link>
+        <Duplicates snippetNews={snippetNews}/>
       </Space>
     </Card>
   );
