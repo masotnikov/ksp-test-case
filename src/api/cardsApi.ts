3576a833a476c8dcca2cacbe75adc99139b0e3d8
masotnikov <masotnikov11@yandex.ru>
2025-05-10 23:42:40 +0300

    feat: add cars API

--- src/api/cardsApi.ts
+++ src/api/cardsApi.ts
@@ -1,7 +1,7 @@
 export const fetchCards = async () => {
-  const response = await fetch('/db.json');
+  const response = await fetch("/db.json");
   if (!response.ok) {
-    throw new Error('Failed to load cards');
+    throw new Error("Failed to load cards");
   }
   const data = await response.json();
   return data.cards;
