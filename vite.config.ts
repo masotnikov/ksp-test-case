385606a8e254c43b1143948d6bbf2efd98919fa2
masotnikov <masotnikov11@yandex.ru>
2025-05-11 16:25:32 +0300

    chore: deployment preparation

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/ksp-test-case/", // имя репозитория на GitHub
+  base: "ksp-test-case",
   plugins: [react()],
 });
